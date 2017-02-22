6736d32e8821333221ff334c0e70ca4e2bddd6c3
Jack Valmadre <jack.valmadre@gmail.com>
2017-02-22 11:19:19 +0000

    Unify functions that call ffmpeg in clips.py

--- clips.py
+++ clips.py
@@ -61,18 +61,21 @@ def main():
             tmp_dir = os.path.join(args.tmp_dir, '%s-%s' % (video_id, track_str))
             create_tmp_dir(tmp_dir)
 
+            out_path = None
             if args.video:
                 # Use user-specified extension if given.
                 if args.video_ext:
-                    ext = args.video_ext
-                    if ext[0] != '.':
-                        ext = '.' + ext
+                    ext = ensure_starts_with_dot(args.video_ext)
                 else:
                     _, ext = os.path.splitext(video_file)
                 out_path = os.path.join(tmp_dir, track_str+ext)
-                cut_video_interval(out_path, video_path, start, end, max_size=args.max_size, codec=args.video_codec)
             else:
-                decode_frames_interval(tmp_dir, video_path, start, end, max_size=args.max_size)
+                out_path = os.path.join(tmp_dir, '%08d.jpg')
+
+            cut_video_interval(out_path, video_path, start, end,
+                               video=args.video,
+                               max_size=args.max_size,
+                               codec=args.video_codec)
 
             # Write CSV file
             boxes_file = os.path.join(tmp_dir, 'boxes.csv')
@@ -98,47 +101,49 @@ def split_tracks(r):
         d.setdefault(video_id, {}).setdefault(track_id, []).append(row)
     return d
 
-def cut_video_interval(dst_file, src_file, start, end, max_size=0, codec=None):
+def cut_video_interval(dst_file, src_file, start, end, video=False, max_size=0, codec=None):
     start_num = round(start * 30)
     num_frames = round(30 * (end-start)) + 1
     # Unless otherwise specified, use same codec as input.
     if not codec:
         codec = 'copy'
+
     size_args = []
     if max_size:
-        size_args = ['-vf', ('scale=' +
-            'w=2*trunc((1+min(iw\,max({0}\,iw*{0}/ih)))/2):' +
-            'h=2*trunc((1+min(ih\,max(ih*{0}/iw\,{0})))/2)').format(max_size)]
-    args = (
-        ['-nostdin',
-        '-accurate_seek',
-        '-ss', str(start),
-        '-t', str(end - start),
-        '-i', src_file,
-        '-vcodec', codec,
-        '-an'] +
-        size_args +
-        [dst_file])
-    status = subprocess.call(['ffmpeg'] + args)
-    if status != 0:
-        raise ValueError('ffmpeg exit status non-zero: %s' % str(status))
+        w_str = 'min(iw\,max({0}\,iw*{0}/ih))'.format(max_size)
+        h_str = 'min(ih\,max(ih*{0}/iw\,{0}))'.format(max_size)
+        if video:
+            # Round to nearest multiple of 2.
+            w_str = '2*trunc((1+({}))/2)'.format(w_str)
+            h_str = '2*trunc((1+({}))/2)'.format(h_str)
+        size_args = ['-vf', 'scale=w={}:h={}'.format(w_str, h_str)]
 
-def decode_frames_interval(dst_dir, video_file, start, end, max_size=0):
-    # TODO: Should check if dst_file contains '%'
-    start_num = round(start * 30)
-    num_frames = round(30 * (end-start)) + 1
-    # Should check if dst_dir contains '%'
-    status = subprocess.call(['ffmpeg',
-        '-v', 'warning',
-        '-accurate_seek',
-        '-ss', str(start),
-        '-i', video_file,
-        '-r', '30',
-        '-vframes', str(num_frames),
-        '-q:v', '2',
-        '-vf', 'scale=w=min(iw\,max(360\,iw*360/ih)):h=min(ih\,max(ih*360/iw\,360))',
-        '-start_number', str(start_num),
-        os.path.join(dst_dir, '%08d.jpg')])
+    if video:
+        input_args = [
+            '-accurate_seek',
+            '-ss', str(start),
+            '-t', str(end - start),
+        ]
+        output_args = [
+            '-vcodec', codec,
+            '-an',
+        ]
+        output_args += size_args
+    else:
+        input_args = [
+            '-accurate_seek',
+            '-ss', str(start),
+        ]
+        output_args = [
+            '-r', '30',
+            '-vframes', str(num_frames),
+            '-q:v', '2',
+            '-start_number', str(start_num),
+        ]
+        output_args += size_args
+
+    command = ['echo', 'ffmpeg', '-nostdin'] + input_args + ['-i', src_file] + output_args + [dst_file]
+    status = subprocess.call(command)
     if status != 0:
         raise ValueError('ffmpeg exit status non-zero: %s' % str(status))
 
@@ -153,6 +158,11 @@ def clear_dir(d):
     fs = os.listdir(d)
     for f in fs:
         os.remove(os.path.join(d, f))
+
+def ensure_starts_with_dot(ext):
+    if ext[0] == '.':
+        return ext
+    return '.' + ext
 
 if __name__ == '__main__':
     main()
